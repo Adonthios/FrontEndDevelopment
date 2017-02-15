ea0cad7235776aeb67e13dc72d751184bc05c49e
Lars <lars.timmerman@student.hu.nl>
2017-02-15 16:16:35 +0100

    Dummy files

---
+++ app/src/main/java/nl/hu/frontenddevelopment/Controller/Adapter.java
@@ -0,0 +1,8 @@
+package nl.hu.frontenddevelopment.Controller;
+
+/**
+ * Created by Lars on 2/15/2017.
+ */
+
+public class Adapter {
+}
---
+++ app/src/main/java/nl/hu/frontenddevelopment/Model/Model.java
@@ -0,0 +1,8 @@
+package nl.hu.frontenddevelopment.Model;
+
+/**
+ * Created by Lars on 2/15/2017.
+ */
+
+public class Model {
+}
