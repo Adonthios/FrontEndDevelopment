e9d340f37e0efdc64b17268987fbfd14c75961ce
Lars <lars.timmerman@student.hu.nl>
2017-02-15 15:15:06 +0100

    Added Structures and LoginActivity

--- app/src/main/java/nl/hu/frontenddevelopment/View/MainActivity.java
+++ app/src/main/java/nl/hu/frontenddevelopment/View/MainActivity.java
@@ -1,4 +1,4 @@
-package nl.hu.frontenddevelopment;
+package nl.hu.frontenddevelopment.View;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import nl.hu.frontenddevelopment.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
