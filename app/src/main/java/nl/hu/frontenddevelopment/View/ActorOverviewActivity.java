69074273c9296dcf4fe21943fb48dd3dd320ba46
Lars <lars.timmerman@student.hu.nl>
2017-03-14 14:03:48 +0100

    Projects can be deleted and are editable.

---
+++ app/src/main/java/nl/hu/frontenddevelopment/View/ActorOverviewActivity.java
@@ -0,0 +1,15 @@
+package nl.hu.frontenddevelopment.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nl.hu.frontenddevelopment.R;
+
+public class ActorOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actor_overview);
+    }
+}
